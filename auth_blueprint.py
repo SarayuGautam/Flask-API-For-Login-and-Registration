ff7f06b5dfec2515557bb854cc70085fa5dc9524
Sarayu Gautam <sarayugautam1@gmail.com>
2021-03-02 10:24:41 +0545

    Refactoring done imports added

--- auth_blueprint.py
+++ auth_blueprint.py
@@ -1,4 +1,11 @@
 from flask import Blueprint, request, Response, jsonify
+from utils import (
+    validate_user_input,
+    generate_salt,
+    generate_hash,
+    db_write,
+    validate_user,
+)
 
 authentication = Blueprint("authentication",__name__)
 
